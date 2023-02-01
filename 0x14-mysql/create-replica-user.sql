83ffcf83e551588c6d499f91c8d9a38424cc2b25
itayi98 <106750793+itayi98@users.noreply.github.com>
2023-02-01 22:00:00 +0200

    Update create-replica-user.sql

--- 0x14-mysql/create-replica-user.sql
+++ 0x14-mysql/create-replica-user.sql
@@ -1,13 +1,8 @@
--- Create db on the master server
-CREATE DATABASE IF NOT EXISTS tyrell_corp;
+-- Create user to check DB status 
+-- Run on the master server
 
--- Create table
-CREATE TABLE IF NOT EXISTS tyrell_corp.nexus6
-(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-);
-
--- Insert an entry
-INSERT INTO `tyrell_corp.nexus6` (`name`) VALUES ("Emmastro");
+CREATE USER IF NOT EXISTS 'replica_user'@'%'
+IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT  REPLICATION CLIENT, SELECT ON *.*
+TO 'replica_user'@'%';
+FLUSH PRIVILEGES;
